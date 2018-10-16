15dfb81d1b0ac738e77f17e9d7a590a6d4395353
heignamerican <namerican.higenovo@gmail.com>
2018-10-16 22:30:30 +0900

    リファクタリング コメント修正・変数修正

--- GFImageCollage/src/heignamerican/gfgazou/heiwatansan/Tansan.java
+++ GFImageCollage/src/heignamerican/gfgazou/heiwatansan/Tansan.java
@@ -14,10 +14,9 @@ import heignamerican.gfgazou.base.UNextBase;
 public class Tansan implements UNext {
 	@Override
 	public void gazou(Files... filesList) throws Exception {
-		// final Files files = filesList[0];
 		final Files files = new Files(this, filesList[0].getSourceFile().getName(), Files.EXTENSION_PNG);
 
-		// 通常の炭酸の2倍の画像を用意して item2x.png に置く
+		// 通常の本気炭酸の2倍の画像を用意して item2x.png に置く。 勇気炭酸は太さが違うので適さない
 		final UNextBase unextbase = new UNextBase(this.getClass(), "item2x.png", new String[] {
 		});
 
@@ -30,15 +29,15 @@ public class Tansan implements UNext {
 				240, 156,
 				162, 82,
 				Orientation.Landscape,
-				RotateDirection.Clockwise);
+				RotateDirection.None);
 
 		ImageIO.write(ImageUtils.scale(unextbase.createUNext(add), 0.5), files.getExtension(), files.getDestFile());
 	}
 
-	// カーブさせる
+	// 曲面に沿わせるようにカーブさせる
 	private BufferedImage convert(final BufferedImage source) {
 		final double rate = 1.08;
-		final int curveRate = 2;
+		final int exponent = 2;
 
 		final BufferedImage result = new BufferedImage((int) (source.getWidth() * rate), source.getHeight(), BufferedImage.TYPE_4BYTE_ABGR);
 
@@ -47,7 +46,7 @@ public class Tansan implements UNext {
 
 		for (int y = 0; y < source.getHeight(); y++) {
 			double a = (1.0 * Math.abs(y - midY) / midY);
-			final int offset = (int) (Math.pow(a, curveRate) * maxOffset);
+			final int offset = (int) (Math.pow(a, exponent) * maxOffset);
 
 			for (int x = 0; x < source.getWidth(); x++) {
 				int newX = x + offset;
